25f481357214de33f5560e0944280458a4072eff
Thomas <prefontainethomas@gmail.com>
2023-01-29 15:52:32 -0800

    syncing with master

--- GitTest3/Program.cs
+++ GitTest3/Program.cs
@@ -13,9 +13,7 @@ namespace GitTest3
             Console.Write("GitTest3");
             Console.Write("This took way too long");
             Console.Write("Deleted File");
-            Console.Write("From GitTest3_local");
             Console.Write("Added in Github");
-            Console.Write("Still can't push.");
             Console.Write("Added from GitTest3_local in Github");
             Console.Read();
         }
